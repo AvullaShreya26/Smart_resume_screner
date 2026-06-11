a64c569bc258329af71d0b533bc322500ef4a47c
AvullaShreya26 <23b81a6650@cvr.ac.in>
2026-06-11 12:04:20 +0530

    Fixed NLTK deployment issue

--- modules/preprocessing.py
+++ modules/preprocessing.py
@@ -1,15 +1,47 @@
 import nltk
+
+# Download required resources
+try:
+    nltk.data.find("tokenizers/punkt")
+except LookupError:
+    nltk.download("punkt")
+
+try:
+    nltk.data.find("tokenizers/punkt_tab")
+except LookupError:
+    nltk.download("punkt_tab")
+
+try:
+    nltk.data.find("corpora/stopwords")
+except LookupError:
+    nltk.download("stopwords")
+
+try:
+    nltk.data.find("corpora/wordnet")
+except LookupError:
+    nltk.download("wordnet")
+
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
 
-nltk.download("punkt", quiet=True)
-nltk.download("stopwords", quiet=True)
-nltk.download("wordnet", quiet=True)
 
 def preprocess_text(text):
+
     tokens = word_tokenize(text.lower())
+
     stop_words = set(stopwords.words("english"))
-    words = [w for w in tokens if w.isalpha() and w not in stop_words]
+
+    words = [
+        word for word in tokens
+        if word.isalpha() and word not in stop_words
+    ]
+
     lemmatizer = WordNetLemmatizer()
-    return " ".join(lemmatizer.lemmatize(w) for w in words)
+
+    clean_words = [
+        lemmatizer.lemmatize(word)
+        for word in words
+    ]
+
+    return " ".join(clean_words)
